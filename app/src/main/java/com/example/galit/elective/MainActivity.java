c71206b358a13c69f8e6eb0fd4f538b1b6ae4171
galitn <galit.naim@gmail.com>
2016-03-17 18:19:58 +0200

    UI: validating forms and improving UI

--- app/src/main/java/com/example/galit/elective/MainActivity.java
+++ app/src/main/java/com/example/galit/elective/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.ImageButton;
 
@@ -16,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
     //this method is activated when sign in is clicked. the method opens signIn activity
